b821afb953e33fdaebcb9d9a2b4497d8c978f3a2
David Aslan <webmaster@aston9.com>
2022-01-16 13:03:47 +0100

    Adding support for threding

--- WakeOnLanRepeat.cs
+++ WakeOnLanRepeat.cs
@@ -1,15 +1,52 @@
-using System;
+﻿using System;
 using EasyWakeOnLan;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WakeOnLanRepeat
 {
     class WakeOnLanRepeat
     {
+        private static Timer timer;
+
         static void Main(string[] args)
         {
             RepeatEveryMinutes(10);
          
         }
+
+        static void RepeatEveryMinutes(int minutes)
+        {
+            var timerState = new TimerState { Counter = 0 };
+            minutes = minutes * 60 * 1000; 
+
+            timer = new Timer(
+                callback: new TimerCallback(TimerTask),
+                state: timerState,
+                dueTime: 1000,
+                period: minutes);
+
+            while (timerState.Counter < 6)
+            {
+                Task.Delay(1000).Wait();
+            }
+
+            timer.Dispose();
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-d HH:mm:ss}: done.");
+        }
+
+        private static void TimerTask(object timerState)
+        {
+            Console.WriteLine($"{DateTime.Now:yyyy-MM-d HH:mm:ss}: starting a new callback.");
+            var state = timerState as TimerState;
+            Interlocked.Increment(ref state.Counter);
+        }
+
+        class TimerState
+        {
+            public int Counter;
+        }
+
 
         static void test()
         {
